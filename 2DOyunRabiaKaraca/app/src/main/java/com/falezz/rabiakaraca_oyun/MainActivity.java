73176188541818b5e3db8b07467e016ec20040bc
Bilgisayar Muhendisligi Dersleri <74094387+bmdersleri@users.noreply.github.com>
2021-06-10 20:02:07 +0300

    2DOyunRabiaKaraca

---
+++ 2DOyunRabiaKaraca/app/src/main/java/com/falezz/rabiakaraca_oyun/MainActivity.java
@@ -0,0 +1,29 @@
+package com.falezz.rabiakaraca_oyun;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button buttonBasla;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonBasla = findViewById(R.id.buttonBasla);
+
+        buttonBasla.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,OyunEkrani.class));
+
+            }
+        });
+
+    }
+
+}
